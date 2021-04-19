3fdb8a33695cbbdbb23dd5615a08e10d201c0225
Kyle Kotowick <kyle@kotowick.ca>
2021-04-19 15:59:47 -0400

    Implement CreateAsset, RefreshAsset, and FilterAsset functions

--- plaid/assets.go
+++ plaid/assets.go
@@ -32,6 +32,73 @@ type AssetReportUser struct {
 	SSN         string `json:"ssn"`
 }
 
+type createAssetReportRequestOptions struct {
+	ClientReportID string           `json:"client_report_id,omitempty"`
+	Webhook        string           `json:"webhook,omitempty"`
+	User           *AssetReportUser `json:"user,omitempty"`
+}
+
+type createAssetReportRequest struct {
+	ClientID      string                          `json:"client_id"`
+	Secret        string                          `json:"secret"`
+	AccessTokens  []string                        `json:"access_tokens"`
+	DaysRequested int                             `json:"days_requested"`
+	Options       createAssetReportRequestOptions `json:"options,omitempty"`
+}
+
+type CreateAssetReportOptions struct {
+	DaysRequested  int
+	ClientReportID string
+	Webhook        string
+	User           *AssetReportUser
+}
+
+type CreateAssetReportResponse struct {
+	APIResponse
+	AssetReportToken string `json:"asset_report_token"`
+	AssetReportID    string `json:"asset_report_id"`
+}
+
+type refreshAssetReportRequestOptions struct {
+	ClientReportID string           `json:"client_report_id,omitempty"`
+	Webhook        string           `json:"webhook,omitempty"`
+	User           *AssetReportUser `json:"user,omitempty"`
+}
+
+type refreshAssetReportRequest struct {
+	ClientID         string                           `json:"client_id"`
+	Secret           string                           `json:"secret"`
+	AssetReportToken string                           `json:"asset_report_token"`
+	DaysRequested    int                              `json:"days_requested,omitempty"`
+	Options          refreshAssetReportRequestOptions `json:"options,omitempty"`
+}
+
+type RefreshAssetReportResponse struct {
+	APIResponse
+	AssetReportToken string `json:"asset_report_token"`
+	AssetReportID    string `json:"asset_report_id"`
+}
+
+type RefreshAssetReportOptions struct {
+	DaysRequested  int
+	ClientReportID string
+	Webhook        string
+	User           *AssetReportUser
+}
+
+type filterAssetReportRequest struct {
+	ClientID            string   `json:"client_id"`
+	Secret              string   `json:"secret"`
+	AssetReportToken    string   `json:"asset_report_token"`
+	AccountIDsToExclude []string `json:"account_ids_to_exclude"`
+}
+
+type FilterAssetReportResponse struct {
+	APIResponse
+	AssetReportToken string `json:"asset_report_token"`
+	AssetReportID    string `json:"asset_report_id"`
+}
+
 type getAssetReportRequest struct {
 	ClientID         string `json:"client_id"`
 	Secret           string `json:"secret"`
@@ -65,6 +132,77 @@ type createAuditCopyRequest struct {
 type CreateAuditCopyTokenResponse struct {
 	APIResponse
 	AuditCopyToken string `json:"audit_copy_token"`
+}
+
+func (c *Client) CreateAssetReportWithOptions(accessTokens []string, options CreateAssetReportOptions) (resp CreateAssetReportResponse, err error) {
+	req := createAssetReportRequest{
+		ClientID:      c.clientID,
+		Secret:        c.secret,
+		AccessTokens:  accessTokens,
+		DaysRequested: options.DaysRequested,
+		Options: createAssetReportRequestOptions{
+			ClientReportID: options.ClientReportID,
+			Webhook:        options.Webhook,
+			User:           options.User,
+		},
+	}
+
+	jsonBody, err := json.Marshal(req)
+	if err != nil {
+		return resp, err
+	}
+
+	err = c.Call("/asset_report/create", jsonBody, &resp)
+	return resp, err
+}
+
+func (c *Client) CreateAssetReport(accessTokens []string) (resp CreateAssetReportResponse, err error) {
+	return c.CreateAssetReportWithOptions(accessTokens, CreateAssetReportOptions{
+		DaysRequested: 30,
+	})
+}
+
+func (c *Client) RefreshAssetReportWithOptions(assetReportToken string, options RefreshAssetReportOptions) (resp RefreshAssetReportResponse, err error) {
+	req := refreshAssetReportRequest{
+		ClientID:         c.clientID,
+		Secret:           c.secret,
+		AssetReportToken: assetReportToken,
+		DaysRequested:    options.DaysRequested,
+		Options: refreshAssetReportRequestOptions{
+			ClientReportID: options.ClientReportID,
+			Webhook:        options.Webhook,
+			User:           options.User,
+		},
+	}
+
+	jsonBody, err := json.Marshal(req)
+	if err != nil {
+		return resp, err
+	}
+
+	err = c.Call("/asset_report/refresh", jsonBody, &resp)
+	return resp, err
+}
+
+func (c *Client) RefreshAssetReport(assetReportToken string) (resp RefreshAssetReportResponse, err error) {
+	return c.RefreshAssetReportWithOptions(assetReportToken, RefreshAssetReportOptions{})
+}
+
+func (c *Client) FilterAssetReport(assetReportToken string, accountIDsToExclude []string) (resp FilterAssetReportResponse, err error) {
+	req := filterAssetReportRequest{
+		ClientID:            c.clientID,
+		Secret:              c.secret,
+		AssetReportToken:    assetReportToken,
+		AccountIDsToExclude: accountIDsToExclude,
+	}
+
+	jsonBody, err := json.Marshal(req)
+	if err != nil {
+		return resp, err
+	}
+
+	err = c.Call("/asset_report/filter", jsonBody, &resp)
+	return resp, err
 }
 
 func (c *Client) GetAssetReport(assetReportToken string) (resp GetAssetReportResponse, err error) {
